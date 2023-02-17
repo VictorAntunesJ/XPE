fcffba8fa042bfb715c463f8633b7f7623885d1c
Antunes <victorsergioantunes23@gmail.com>
2023-02-17 01:57:34 -0300

    Program xpe

--- Models/vazio/06-imc/Js/script.js
+++ Models/vazio/06-imc/Js/script.js
@@ -1,1 +1,32 @@
-console.log('Teste no console do JavaScript');
+function start() {
+  var buttonCalculateIMC = document.querySelector('#button-calculate-imc');
+  buttonCalculateIMC.addEventListener('click', handleButtonClick);
+
+  var inputWeight = document.querySelector('#input-weight');
+  var inputHeight = document.querySelector('#input-height');
+
+  inputWeight.addEventListener('input', handleButtonClick)
+  inputHeight.addEventListener('input', handleButtonClick)
+
+  handleButtonClick();
+}
+
+function CalculateIMC(weight, height) {
+  return weight / (height * height);
+}
+
+function handleButtonClick() {
+  var inputWeight = document.querySelector('#input-weight');
+  var inputHeight = document.querySelector('#input-height');
+
+  var imcResult = document.querySelector('#imc-result');
+
+  var weight = Number(inputWeight.value);
+  var height = Number(inputHeight.value);
+
+  var imc = CalculateIMC(weight, height);
+  var formattedImc = imc.toFixed(2).replace('.', ',');
+  imcResult.textContent = formattedImc;
+}
+
+start();
---
+++ Models/vazio/07-mega-sena-v1/Js/script.js
@@ -0,0 +1,184 @@
+var board = [];
+var currentGame = [1, 5, 11, 13, 15, 17];
+var saveGames = [];
+
+var state = { board: [], currentGames: [], saveGames: [] };
+
+function start() {
+  createBoard();
+  newGame();
+}
+
+function createBoard() {
+  state.board = [];
+
+  for (var i = 1; i <= 60; i++) {
+    state.board.push(i);
+  }
+}
+
+function newGame() {
+  resetGames();
+  render();
+
+  console.log(state.currentGames);
+}
+
+function render() {
+  renderBoard();
+  renderButtons();
+  renderSavedGames();
+}
+
+function renderBoard() {
+  var divBoard = document.querySelector('#Megasena-board');
+  divBoard.innerHTML = '';
+
+  var ulNumbers = document.createElement('ul');
+  ulNumbers.classList.add('numbers');
+
+  for (var i = 0; i < state.board.length; i++) {
+    var currentNumber = state.board[i];
+
+    var liNumber = document.createElement('li');
+    liNumber.textContent = currentNumber;
+
+    liNumber.addEventListener('click', handleNumberClick);
+
+    ulNumbers.appendChild(liNumber);
+  }
+  divBoard.appendChild(ulNumbers);
+}
+
+function handleNumberClick(event) {
+  var value = Number(event.currentTarget.textContent);
+
+  if (isNumberInGame(value)) {
+    removeNumberFromGame(value);
+  } else {
+    addNumberToGame(value);
+  }
+
+  console.log(state.currentGames);
+}
+
+function renderButtons() {
+  var divButtons = document.querySelector('#Megasena-buttons');
+  divButtons.innerHTML = '';
+
+  var buttonNewGame = createNewGameButton();
+  var buttonRandomGame = createRandomGameButton();
+  var buttonSaveGame = createSaveGameButton();
+
+  divButtons.appendChild(buttonNewGame);
+  divButtons.appendChild(buttonRandomGame);
+  divButtons.appendChild(buttonSaveGame);
+}
+
+function createSaveGameButton() {
+  var button = document.createElement('button');
+  button.textContent = 'Salvar jogo';
+
+  button.addEventListener('click', saveGame);
+
+  return button;
+}
+
+function createRandomGameButton() {
+  var button = document.createElement('button');
+  button.textContent = 'Jogo aleatório';
+
+  button.addEventListener('click', randomGame);
+
+  return button;
+}
+
+function createNewGameButton() {
+  var button = document.createElement('button');
+  button.textContent = 'Novo Jogo';
+
+  button.addEventListener('click', newGame);
+
+  return button;
+}
+
+function renderSavedGames() {}
+
+function addNumberToGame(numberToAdd) {
+  if (numberToAdd < 1 || numberToAdd > 60) {
+    console.error('Número inválido', numberToAdd);
+    return;
+  }
+
+  if (state.currentGames.length >= 6) {
+    console.error('O jogo ja está completo');
+    return;
+  }
+
+  if (isNumberInGame(numberToAdd)) {
+    console.error('Este número já está no jogo. ', numberToAdd);
+    return;
+  }
+
+  state.currentGames.push(numberToAdd);
+}
+
+function removeNumberFromGame(numberToRemove) {
+  if (numberToRemove < 1 || numberToRemove > 60) {
+    console.error('Número inválido', numberToRemove);
+    return;
+  }
+
+  var newGame = [];
+
+  for (var i = 0; i < state.currentGames.length; i++) {
+    var currentNumber = state.currentGames[i];
+
+    if (currentNumber === numberToRemove) {
+      continue;
+    }
+
+    newGame.push(currentNumber);
+  }
+  state.currentGames = newGame;
+}
+
+function isNumberInGame(numberToCheck) {
+  // if (state.currentGames.includes(numberToCheck)) {
+  //   return true;
+  // }
+  // return false;
+
+  return state.currentGames.includes(numberToCheck);
+}
+
+function saveGame() {
+  if (!isGameComplete()) {
+    console.error('O jogo não está completo!');
+    return;
+  }
+
+  state.saveGames.push(state.currentGames);
+  newGame();
+
+  console.log(state.saveGames);
+}
+
+function isGameComplete() {
+  return state.currentGames.length === 6;
+}
+
+function resetGames() {
+  state.currentGames = [];
+}
+
+function randomGame() {
+  resetGames();
+
+  while (!isGameComplete()) {
+    var randomNumber = Math.ceil(Math.random() * 60);
+    addNumberToGame(randomNumber);
+  }
+  console.log(state.currentGames);
+}
+start();
